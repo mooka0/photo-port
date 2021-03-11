441886290d2e6ec26a3f7525cffc6f20fb833b20
mooka0 <angelankao@gmail.com>
2021-03-11 13:37:09 -0800

    set up about and nav components

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import { capitalizeFirstLetter } from "../../assets/utils/helper";
 
 function Nav() {
   const  categories = [
